4dd01a8d742cbc594c2ceee9e7e18c19177fd568
jagadeesh-python <87078028+jagadeesh-python@users.noreply.github.com>
2023-09-05 12:29:15 +0530

    variables/keywords

---
+++ variables/ex1.py
@@ -0,0 +1,22 @@
+#variables
+#a=10
+#print(a)
+
+#name='jagadeesh'
+#print(name)
+
+#variable name should not starts with a numbers and symbols
+#3name='bangalore'
+#print(3name)
+
+#variable name should not have white spaces
+#na me='hyderabad'
+#print(na me)
+
+#we can take underscore in the variable name
+nam_e='hari'
+print(nam_e)
+
+#variable name should not contain special characters
+#sad@#j=32487
+#print(sad@#j)
---
+++ variables/ex2.py
@@ -0,0 +1,5 @@
+#keywords
+#python has two types of keywords:
+#pre-defined keywords/built-in keywords'
+#user defined keywords
+
