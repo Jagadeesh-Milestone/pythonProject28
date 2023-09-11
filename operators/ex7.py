3e858949a085741a86056c940e6d51b96f336ca0
jagadeesh-python <87078028+jagadeesh-python@users.noreply.github.com>
2023-09-11 12:41:22 +0530

    operators

---
+++ operators/ex7.py
@@ -0,0 +1,21 @@
+#bitwise
+#bitwise and(&)
+a=26
+print(bin(a))
+b=29
+print(bin(b))
+print(a&b)
+print(bin(24))
+#26--ob11010
+#29--0b11101
+#------------
+#24--0b11000
+#31--0b11111
+
+#bitwise or(|)
+a=26
+print(bin(a))
+b=29
+print(bin(b))
+print(a|b)
+print(bin(31))
